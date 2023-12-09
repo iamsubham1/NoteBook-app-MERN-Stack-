4fbe0e39069b5629303de60acaaea48b1a993195
subham das <das1998lipun@gmail.com>
2023-12-09 16:59:30 +0530

    added comments

--- backend/routes/note.js
+++ backend/routes/note.js
@@ -2,23 +2,27 @@
 
 const express = require('express');
 const router = express.Router()
+
+//import the noteschema
 const Notes = require('../models/NoteSchema');
+//import express validator
 const { body, validationResult } = require('express-validator');
 
-
+//response on /addnote end point, validation check 
 router.post('/addnote', [body('title', 'title is required').exists(),
 body('description', 'title is required').exists(),
 body('tag', 'title is required').exists()],
     async (req, res) => {
         const errors = validationResult(req)
         try {
+            //validation results
             if (!errors.isEmpty) {
                 const errorMessages = await errors.array().map(error => {
                     return { msg: `${error.msg}` }
                 })
                 return res.status(400).json({ error: errorMessages })
             }
-
+            //check for same existing titles
             let title = await Notes.findOne({ title: req.body.title })
             if (title) {
                 return res.status(400).json({ msg: "title exists" })
