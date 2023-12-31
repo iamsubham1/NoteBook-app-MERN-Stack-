bbd9a9abc8ca55cfcb0177469b36b2efd5333086
subham das <das1998lipun@gmail.com>
2023-12-31 23:49:22 +0530

    contact page creation

--- src/components/ContactPage.jsx
+++ src/components/ContactPage.jsx
@@ -1,11 +1,18 @@
 import React from 'react'
 import '../components/css/Contact.css'
+import Vector from '../assets/Get in touch-cuate.svg'
 
 const ContactPage = () => {
     return (
         <div id='page-container'>
-
-
+            <img src={Vector} alt='vector'></img>
+            <div className='contact-section'>
+                <h1>Contact Me !</h1>
+                <div id='Contact-form'>contact form</div>
+                <ul><li>linkedin</li>
+                    <li>github</li>
+                    <li>whatsapp</li></ul>
+            </div>
         </div>
     )
 }
