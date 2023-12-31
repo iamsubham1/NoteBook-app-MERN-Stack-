1ee5d389d554bb5b1ac6bc7f6efe347909584350
subham das <das1998lipun@gmail.com>
2024-01-01 00:34:05 +0530

    completed contact page

--- src/components/ContactPage.jsx
+++ src/components/ContactPage.jsx
@@ -23,36 +23,62 @@ const ContactPage = () => {
     };
     return (
         <div id='page-container'>
-            <img src={Vector} alt='vector'></img>
+            <div id='image-section'><img src={Vector} alt='vector'></img></div>
+
             <div id='contact-section'>
                 <h1>Contact Me !</h1>
-                <div id='Contact-form'><form onSubmit={handleSubmit}>
-                    <label htmlFor="email">Email:</label>
-                    <input
-                        type="email"
-                        id="email"
-                        name="email"
-                        value={formData.email}
-                        onChange={handleChange}
-                        required
-                    />
+                <div id='Contact-form'><form className='loginForm' >
 
-                    <label htmlFor="message">Message:</label>
-                    <textarea
-                        id="message"
-                        name="message"
-                        value={formData.message}
-                        onChange={handleChange}
-                        rows="4"
-                        cols="50"
-                        required
-                    ></textarea>
+                    <div id='Textfield'>
 
-                    <button type="submit">Submit</button>
+                        <div className="mb-3">
+                            <label htmlFor="email" className="form-label" id='label'>
+                                Email address
+                            </label>
+                            <input
+                                type="email"
+                                name='email'
+                                className="form-control"
+                                id="email"
+                                aria-describedby="emailHelp"
+                            // value={userData.email}
+                            // onChange={handleInput}
+                            />
+                        </div>
+                        <div className="mb-3">
+                            <label htmlFor="message" className="form-label">
+                                message
+                            </label>
+                            <input
+                                type="message"
+                                name='message'
+                                className="form-control"
+                                id="message"
+                            // value={userData.password}
+                            // onChange={handleInput}
+                            />
+                        </div>
+                    </div>
+                    <div id='lower-section'>
+                        <button
+                            type="submit"
+                            className="btn btn-primary"
+                            id='submit-btn'
+                        // disabled={!userData.email || !userData.password}
+                        >
+                            submit
+                        </button>
+
+                    </div>
                 </form></div>
-                <ul><li>linkedin</li>
-                    <li>github</li>
-                    <li>whatsapp</li></ul>
+                <ul id='link'>
+
+                    <li><i class="fa-brands fa-linkedin"></i></li>
+                    <li><i class="fa-brands fa-github"></i></li>
+                    <li><i class="fa-brands fa-whatsapp"></i></li>
+
+
+                </ul>
             </div>
         </div>
     )
