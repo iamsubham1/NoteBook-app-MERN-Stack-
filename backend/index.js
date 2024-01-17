8b77b3e5091b2a25701cd16b0d624c286833af4c
subham das <das1998lipun@gmail.com>
2024-01-17 21:31:31 +0530

    origin change

--- backend/index.js
+++ backend/index.js
@@ -6,7 +6,7 @@ const cors = require('cors')
 const cookieParser = require('cookie-parser');
 
 const corsOptions = {
-    origin: '*',
+    origin: 'https://myquickmemo.netlify.app',
     methods: "GET,POST,PUT,PATCH,DELETE,HEAD",
     credentials: true
 }
